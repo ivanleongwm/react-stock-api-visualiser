171c73b078279a6864b87c3935bd74b548ef408d
ivanleongwm <ivanleongwm@gmail.com>
2022-02-19 15:12:54 +0800

    test ghpages

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
     <div className="App">
       <div className="content-container">
         <div>{stock.price}</div>
+        <div>test</div>
         <Header />
         <Graphs />
         <Cards stock={stock}/>
