a379280f4553c9916b927bdfd5b63414d6db6442
ivanleongwm <ivanleongwm@gmail.com>
2022-02-19 09:10:10 +0800

    setup basic app and cards components and basic css

---
+++ src/components/card.jsx
@@ -0,0 +1,11 @@
+import './card.css';
+
+export default function Card () {
+    return (
+        <div className="cards-container">
+            <div className="card">World</div>
+            <div className="card">World</div>
+            <div className="card">World</div>
+        </div>
+    )
+}
---
+++ src/components/graphs.jsx
@@ -0,0 +1,8 @@
+export default function Graphs () {
+    return (
+        <div className="graphs-container">
+            <div className="line-graph"></div>
+            <div className="bar-graph"></div>
+        </div>
+    )
+}
