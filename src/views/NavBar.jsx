7d2bc06b9f33e9635e98039f1a1c2ff8b8cdfdd7
ivanleongwm <ivanleongwm@gmail.com>
2022-03-05 13:59:33 +0800

    portfolio cart added

--- src/views/NavBar.jsx
+++ src/views/NavBar.jsx
@@ -1,20 +1,19 @@
 import {Link} from "react-router-dom"
+import './NavBar.css'
 
 function Navbar() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/react-stock-api-visualiser">Main Page</Link>
-                </li>
-                <li>
-                    <Link to="/react-stock-api-visualiser/home">Home Page</Link>
-                </li>
-                <li>
+        <div className="navbarContainer">
+            <div className="linkContainer">
+                <Link to="/react-stock-api-visualiser">Main Page</Link>
+            </div>
+            <div className="linkContainer">
+                <Link to="/react-stock-api-visualiser/home">Your Portfolio</Link>
+            </div>
+            <div className="linkContainer">
                 <Link to="/react-stock-api-visualiser/news">News</Link>
-                </li>
-            </ul>
-        </nav>
+            </div>
+        </div>
     )
 }
 
